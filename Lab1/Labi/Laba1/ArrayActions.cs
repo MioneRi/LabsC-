3460fd6660a8d1312f9e2b1e0bf6efb16d547451
Maks <thebestguy1101001@gmail.com>
2020-03-22 22:15:21 +0300

    Some comment

---
+++ Lab1/Labi/Laba1/ArrayActions.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ArrayActions
+{
+
+
+    public int InputArray(int[,] myArray, int strings, int columns)
+    {
+
+        for (int i = 0; i < strings; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write("a[{0}][{1}] = ", i, j);
+                try
+                {
+                    myArray[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You enter smth wrong!");
+                    return 1;
+                }
+            }
+        }
+        return 0;
+
+    }
+
+    public void PrintArray(int[,] myArray, int strings, int columns)
+    {
+
+        for (int i = 0; i < strings; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(myArray[i, j] + "    ");
+            }
+            Console.Write("\n");
+        }
+
+    }
+
+    public void PrintArray(double[,] myArray, int strings, int columns)
+    {
+
+        for (int i = 0; i < strings; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(myArray[i, j] + "    ");
+            }
+            Console.Write("\n");
+        }
+
+    }
+
+    public void MultiplyArray(int[,] myArray, int strings, int columns, int factor)
+    {
+
+        for (int i = 0; i < strings; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                myArray[i, j] *= factor;
+            }
+        }
+
+    }
+
+    public void TransposeArray(int[,] myArray, int strings, int columns)
+    {
+        int[,] transporedArray = new int[columns, strings];
+
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < strings; j++)
+            {
+                transporedArray[i, j] = myArray[j, i];
+            }
+        }
+
+        PrintArray(transporedArray, columns, strings);
+
+    }
+
+    public void SumArrays(int[,] array1, int[,] array2, int strings, int columns, char sign)
+    {
+        int[,] resultArray = new int[strings, columns];
+
+        if (sign == '+')
+        {
+            for (int i = 0; i < strings; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    resultArray[i, j] = array1[i, j] + array2[i, j];
+                }
+            }
+        }
+        else if (sign == '-')
+        {
+            for (int i = 0; i < strings; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    resultArray[i, j] = array1[i, j] - array2[i, j];
+                }
+            }
+        }
+
+        PrintArray(resultArray, strings, columns);
+    }
+
+    public void MultiplyArrays(int[,] Array1, int[,] Array2, int Strings1, int Columns1, int Strings2, int Columns2)
+    {
+        int[,] ResultArray = new int[Strings1, Columns2];
+
+        for (int i = 0; i < Strings1; i++)
+        {
+            for (int j = 0; j < Columns2; j++)
+            {
+                for (int v = 0; v < Columns1; v++)
+                {
+                    ResultArray[i, j] += Array1[i, v] * Array2[v, j];
+                }
+            }
+        }
+
+        PrintArray(ResultArray, Strings1, Columns2);
+    }
+
+    public int DeterminantArray(int[,] Array, int Size, int FlagSize, int Ji) 
+    {
+        int[,] NewArray = new int[Size, Size];
+
+        if (Size == 1)
+        {
+            return Array[0, 0];
+        }
+
+        if (Size < FlagSize)
+        {
+            int PastSize = Size + 1;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0, Pastj = 0; Pastj < PastSize; Pastj++)
+                {
+                    if (Pastj == Ji)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        NewArray[i, j] = Array[i + 1, Pastj];
+                        j++;
+                    }
+
+                }
+            }
+        }
+        else if (Size == FlagSize)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    NewArray[i, j] = Array[i, j];
+                }
+            }
+        }
+
+        int Answer = 0;
+
+        if (Size == 2)
+        {
+            Answer = NewArray[0, 0] * NewArray[1, 1] - NewArray[0, 1] * NewArray[1, 0];
+        }
+        else
+        {
+            int i = 0;
+            for (int j = 0; j < Size; j++)
+            {
+                Answer += NewArray[i, j] * (int)Math.Pow(-1, i + j) * DeterminantArray(NewArray, Size - 1, Size, j);
+            }
+        }
+
+        return Answer;
+    }
+
+    public void ReverseArray(int[,] Array, int Size)
+    {
+        if (Size == 1)
+        {
+            Console.WriteLine(Math.Pow(Array[0, 0], -1));
+            return;
+        }
+
+        int[,] NewArray = new int[Size, Size]; //Матрица алгебраических дополнений
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                int[,] TempArray = new int[Size - 1, Size - 1];
+
+                for (int i1 = 0, PastI = 0; PastI < Size; PastI++)
+                {
+                    if (PastI != i)
+                    {
+                        for (int j1 = 0, PastJ = 0; PastJ < Size; PastJ++)
+                        {
+                            if (PastJ != j)
+                            {
+                                TempArray[i1, j1] = Array[PastI, PastJ];
+
+                                j1++;
+                            }
+
+                        }
+
+                        i1++;
+
+                    }
+               
+                }
+
+                NewArray[i, j] = (int)Math.Pow(-1, i+j) * DeterminantArray(TempArray,Size-1,Size-1,-1);
+
+            }
+
+        }
+
+        //Сейчас нужно будет транспонировать матрицу алгеб. доп. (тоесть NewArray записать в AnswerArray)
+
+        double[,] AnswerArray = new double[Size, Size];  //Транспонированная матрица от матрицы NewArray
+        int OurDeterminant = DeterminantArray(Array, Size, Size, -1);
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                AnswerArray[i, j] = (double)NewArray[j, i];
+                AnswerArray[i, j] /= (double)OurDeterminant;  //Делим каждый член на Определитель исходной матр.
+            }
+        }
+
+        PrintArray(AnswerArray, Size, Size);
+
+    }
+
+
+
+
+}
+
+
---
+++ Lab1/Labi/Laba1/Program.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;  
+//using static System.Console; 
+
+namespace Laba1          
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t----------------Программа матричный калькулятор-----------------");
+
+            Console.WriteLine("Доступные действия :  ");
+
+            while (true)
+            {
+                Console.WriteLine("1. Умножение на число ");
+                Console.WriteLine("2. Транспонирование ");
+                Console.WriteLine("3. Сумма (разность) ");
+                Console.WriteLine("4. Умножение матриц ");
+                Console.WriteLine("5. Нахождение определителя ");
+                Console.WriteLine("6. Нахождение обратной матрицы ");
+                Console.WriteLine("7. Выход ");
+
+                Console.Write("\nВыберите пункт : ");
+                int Choise;
+
+                try
+                {
+                    Choise = Convert.ToInt32(Console.ReadLine()); ;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You enter smth wrong!\n");
+                    break;
+                }
+
+                int Strings;
+                int Columns;
+
+                switch (Choise)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите размеры матрицы ");
+                            Console.Write("Кол-во строк : ");
+
+                            try
+                            {
+                                Strings = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+                            if (Strings < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+
+                            Console.Write("Кол-во столбцов : ");
+
+                            try
+                            {
+                                Columns = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+                            if (Columns < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Console.Write("Множитель : ");
+                            int Factor;
+
+                            try
+                            {
+                                Factor = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            ArrayActions Action1 = new ArrayActions();
+                            int[,] MyArray = new int[Strings, Columns];
+                            int Test = Action1.InputArray(MyArray, Strings, Columns);
+
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+
+                            Action1.MultiplyArray(MyArray, Strings, Columns, Factor);
+
+                            Console.WriteLine("\nОтвет : ");
+                            Action1.PrintArray(MyArray, Strings, Columns);
+
+                            Console.Write("\n");
+
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Введите размеры матрицы ");
+
+                            Console.Write("Кол-во строк : ");
+                            try
+                            {
+                                Strings = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+                            if (Strings < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Console.Write("Кол-во столбцов : ");
+                            try
+                            {
+                                Columns = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+                            if (Columns < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            ArrayActions Action2 = new ArrayActions();
+                            int[,] MyArray2 = new int[Strings, Columns];
+                            int Test = Action2.InputArray(MyArray2, Strings, Columns);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            Console.WriteLine("Ответ : ");
+                            Action2.TransposeArray(MyArray2, Strings, Columns);
+
+                            Console.Write("\n");
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Введите размеры матриц          \t ***(ввод производится один раз тк они соразмерны)");
+
+                            Console.Write("Кол-во строк : ");
+                            try
+                            {
+                                Strings = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+                            if (Strings < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Console.Write("Кол-во столбцов : ");
+                            try
+                            {
+                                Columns = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+                            if (Columns < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            ArrayActions Action3 = new ArrayActions();
+                            int[,] MyArray3 = new int[Strings, Columns];
+                            int[,] Myarray33 = new int[Strings, Columns];
+
+                            Console.Write("Ввод матрицы 1: \n");
+                            int Test = Action3.InputArray(MyArray3, Strings, Columns);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            Console.WriteLine("Ввод матрицы 2: \n");
+                            Test = Action3.InputArray(Myarray33, Strings, Columns);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            Console.Write("Введите знак (+ or -) : ");
+                            char Sign;
+                            try
+                            {
+                                Sign = Convert.ToChar(Console.ReadLine());
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            while (Sign != '+' && Sign != '-')
+                            {
+                                Console.Write("You enter smth wrong! \n  Try again : ");
+                                Sign = Convert.ToChar(Console.ReadLine());
+                            }
+
+                            Console.WriteLine("Ответ : ");
+                            Action3.SumArrays(MyArray3, Myarray33, Strings, Columns, Sign);
+
+                            Console.Write("\n");
+
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine("Введите размеры матрицы 1 ");
+
+                            Console.Write("Кол-во строк : ");
+                            int Strings1;
+                            try
+                            {
+                                Strings1 = Convert.ToInt32(Console.ReadLine()); ;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!\n");
+                                break;
+                            }
+                            if (Strings1 == 0 || Strings1 == -1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Console.Write("Кол-во столбцов : ");
+                            int Columns1;
+                            try
+                            {
+                                Columns1 = Convert.ToInt32(Console.ReadLine()); ;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!\n");
+                                break;
+                            }
+                            if (Columns1 < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Console.WriteLine("Введите размеры матрицы 2 ");
+
+                            int Strings2 = Columns1;                   //STRINGS2 = COLUMNS
+                            Console.Write("Кол-во строк : {0}           \t***(тоже что и кол-во строк 2ой матрицы)\n", Strings2);
+
+                            Console.Write("Кол-во столбцов : ");
+                            int Columns2;
+                            try
+                            {
+                                Columns2 = Convert.ToInt32(Console.ReadLine()); ;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!\n");
+                                break;
+                            }
+                            if (Columns2 < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            ArrayActions Action4 = new ArrayActions();
+                            int[,] Array1 = new int[Strings1, Columns1];
+                            int[,] Array2 = new int[Strings2, Columns2];
+
+                            Console.WriteLine("Ввод матрицы 1 : ");
+                            int Test;
+                            Test = Action4.InputArray(Array1, Strings1, Columns1);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            Console.WriteLine("Ввод матрицы 2 : ");
+                            Test = Action4.InputArray(Array2, Strings2, Columns2);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            Console.WriteLine("Ответ : ");
+                            Action4.MultiplyArrays(Array1, Array2, Strings1, Columns1, Strings2, Columns2);
+
+                            Console.Write("\n");
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("Введите размеры матрицы ");
+
+                            Console.Write("Кол-во строк : ");
+                            try
+                            {
+                                Strings = Convert.ToInt32(Console.ReadLine()); ;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!\n");
+                                break;
+                            }
+                            if (Strings < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Columns = Strings;
+                            Console.Write("Кол-во столбцов : {0}       \t ***(the same as amount of Strings)\n", Columns);
+
+                            ArrayActions Action5 = new ArrayActions();
+                            int[,] MyArray = new int[Strings, Columns];
+                            int Test = Action5.InputArray(MyArray, Strings, Columns);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            int Answer = Action5.DeterminantArray(MyArray, Strings, Strings, -1);
+
+                            Console.WriteLine("Ответ : {0}", Answer);
+                            Console.Write("\n");
+
+                        }
+                        break;
+                    case 6:
+                        {
+                            Console.WriteLine("Введите размеры матрицы ");
+
+                            Console.Write("Кол-во строк : ");
+                            try
+                            {
+                                Strings = Convert.ToInt32(Console.ReadLine()); ;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("You enter smth wrong!\n");
+                                break;
+                            }
+                            if (Strings < 1)
+                            {
+                                Console.WriteLine("You enter smth wrong!");
+                                break;
+                            }
+
+                            Columns = Strings;
+                            Console.Write("Кол-во столбцов : {0}       \t ***(the same as amount of Strings)\n", Columns);
+
+                            ArrayActions Action6 = new ArrayActions();
+                            int[,] MyArray = new int[Strings, Columns];
+                            int Test = Action6.InputArray(MyArray, Strings, Columns);
+                            if (Test == 1)
+                            {
+                                break;
+                            }
+
+                            int OurDeterminant = Action6.DeterminantArray(MyArray, Strings, Strings, -1);
+
+                            if (OurDeterminant == 0)
+                            {
+                                Console.WriteLine("Обратной матрицы не существует! (Определитель = 0)");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ответ : ");
+
+                                Action6.ReverseArray(MyArray, Strings);
+
+                                Console.Write("\n");
+                            }
+                        }
+
+                        break;
+                    case 7:
+                        {
+                            Console.Write("\n");
+                            return 0;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("You enter smth wrong! \n");
+                        }
+                        break;
+
+                }
+
+
+
+            }
+
+
+
+            return 0;
+        }
+    }
+}
