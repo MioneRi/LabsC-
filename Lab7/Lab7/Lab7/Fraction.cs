ef2d0a94addc6d84ae5f890c3dd8d8e98b12e9c9
Maks <thebestguy1101001@gmail.com>
2020-06-05 03:42:54 +0300

    Lab7 addded

---
+++ Lab7/Lab7/Lab7/Fraction.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Lab7
+{
+    class Fraction : IComparable<Fraction>, IFormattable
+    {
+        // Числитель.
+        public int Numerator
+        {
+            get => numerator;
+            set => SetNumerator(value);
+        }
+
+        // Знаменатель.
+        public int Denominator
+        {
+            get => denominator;
+            set => SetDenomerator(value);
+        }
+
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        // Наибольший общий делитель.
+        public static int GreatestCommonDivisor(int n, int m)
+        {
+            if (n > m)
+            {
+                return EuclidsAlgorithm(n ,m);
+            }
+            else
+            {
+                return EuclidsAlgorithm(m, n);
+            }         
+        }
+
+        private static int EuclidsAlgorithm(int a, int b)
+        {
+            int reminder = a % b;
+
+            if (reminder == 0)
+            {
+                return Math.Abs(b);
+            }
+            else
+            {
+                return EuclidsAlgorithm(b, reminder);
+            }
+
+        }
+
+        private void Reduce()
+        {
+            if (numerator != 0 && denominator != 0)
+            {
+                int ourGCD = GreatestCommonDivisor(numerator, denominator);
+
+                numerator /= ourGCD;
+                denominator /= ourGCD;
+            }         
+        }
+
+        private void SetNumerator(int numerator)
+        {
+            this.numerator = numerator;
+
+            // Reducing the values if it's possible.
+            Reduce();
+        }
+
+        private void SetDenomerator(int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denumerator cannot be a null ...");
+            }
+            else if (denominator < 0)
+            {
+                Numerator = -1 * Numerator;
+                denominator = -1 * denominator;
+            }
+
+            this.denominator = denominator;
+
+            // Reducing if it's possible.
+            Reduce();
+        }
+
+        static int LeastCommonMultiple(int a, int b)
+        {
+            int result = 0;
+
+            result = (a * b) / (GreatestCommonDivisor(a, b));
+
+            return result;
+        }
+
+        public int CompareTo(Fraction ourObj)
+        {
+            int ourLCM = LeastCommonMultiple(Denominator, ourObj.Denominator);
+
+            int res1 = Numerator * (ourLCM/Denominator);
+            int res2 = ourObj.Numerator * (ourLCM/ourObj.Denominator);
+
+            if (res1 > res2)
+            {
+                return 1;
+            }
+            else if (res1 < res2)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return this.ToString("DOT", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        // Formats "DOT" and "DIV"
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (String.IsNullOrEmpty(format))
+            {
+                format = "DOT";
+            }
+
+            if (formatProvider == null)
+            {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+
+            switch (format.ToUpperInvariant())
+            {
+                case "DOT":
+                    {
+                        double tmp = ( (double)Numerator / (double)Denominator );
+
+                        return tmp.ToString();
+                    }
+                    break;
+                case "DIV":
+                    {
+                        string tmp = Numerator.ToString() + "/" + Denominator.ToString();
+
+                        return tmp;
+                    }
+                    break;
+                default:
+                    {
+                        throw new Exception("This format isn't correct...\n");
+                    }
+            }
+        }
+
+        public static Fraction operator +(Fraction fr1, Fraction fr2)
+        {
+            int LCM, newNumerator;
+
+            LCM = LeastCommonMultiple(fr1.denominator, fr2.denominator);
+            newNumerator = fr1.numerator * (LCM / fr1.denominator) + fr2.numerator * (LCM/fr2.denominator);
+
+            return new Fraction(newNumerator, LCM);
+        }
+
+        public static Fraction operator -(Fraction fr)
+        {
+            return new Fraction(-fr.numerator, fr.denominator);
+        }
+
+        public static Fraction operator -(Fraction fr1, Fraction fr2)
+        {
+            // Cuz we already declare operation "+" betweeen them.
+            return fr1 + (-fr2);
+        }
+
+        public static Fraction operator *(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction( (fr1.numerator * fr2.numerator), (fr1.denominator * fr2.denominator) );
+        }
+
+        public static Fraction operator /(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction( (fr1.numerator * fr2.denominator), (fr1.denominator * fr2.numerator) );
+        }
+
+        public static bool operator >(Fraction fr1, Fraction fr2)
+        {
+            return fr1.CompareTo(fr2) == 1;
+        }
+
+        public static bool operator <(Fraction fr1, Fraction fr2)
+        {
+            return fr1.CompareTo(fr2) == -1;
+        }
+
+        public static bool operator ==(Fraction fr1, Fraction fr2)
+        {
+            return fr1.CompareTo(fr2) == 0;
+        }
+
+        public static bool operator !=(Fraction fr1, Fraction fr2)
+        {
+            return fr1.CompareTo(fr2) != 0;
+        }
+
+        public static bool operator >=(Fraction fr1, Fraction fr2)
+        {
+            return fr1.CompareTo(fr2) != -1;
+        }
+
+        public static bool operator <=(Fraction fr1, Fraction fr2)
+        {
+            return fr1.CompareTo(fr2) != 1;
+        }
+
+        public static explicit operator int(Fraction fr)
+        {
+            int ourInt = fr.numerator / fr.denominator;
+
+            return ourInt;
+        }
+
+        public static explicit operator double(Fraction fr)
+        {
+            double ourDbl = ( ((double)fr.numerator) / ((double)fr.denominator) );
+
+            return ourDbl;
+        }
+
+        // Converting from string to Fraction.
+        public static Fraction Parse(string ourStr)
+        {
+            // That string can be "12.4" or "12/4" or "12,4".
+            string patternWithDot = @"^(-?\d*)[\.|,](\d*)$";
+            string patternWithDiv = @"^(-?\d*)\/(\d*)$";
+
+            Regex regWithDot = new Regex(@"^(-?\d*)[\.|,](\d*)$");
+            Regex regWithDiv = new Regex(@"^(-?\d*)\/(\d*)$");
+
+            // Check our situation.       
+            if (Regex.IsMatch(ourStr, patternWithDot, RegexOptions.IgnoreCase))
+            {
+                // Get our correct string.
+                Match ourValue = regWithDot.Match(ourStr);
+                
+                int firstBlock = (int)Math.Pow(10, ourValue.Groups[1].Value.Length);
+                int firstParameter = int.Parse(ourValue.Groups[1].Value) * firstBlock + int.Parse(ourValue.Groups[2].Value);
+                int secondParameter = firstBlock;
+
+                return new Fraction(firstParameter, secondParameter);
+            }
+            else if (Regex.IsMatch(ourStr, patternWithDiv, RegexOptions.IgnoreCase))
+            {
+                Match ourValue = regWithDiv.Match(ourStr);
+
+                int firstParameter = int.Parse(ourValue.Groups[1].Value);
+                int secondParametr = int.Parse(ourValue.Groups[2].Value);
+
+                return new Fraction(firstParameter, secondParametr);
+            }
+            else
+            {
+                throw new Exception("\nUncorrect string...\n");
+            }      
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Fraction fraction) && (numerator == fraction.numerator) && (denominator == fraction.denominator);
+        }
+
+
+
+
+
+    }
+}
---
+++ Lab7/Lab7/Lab7/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("\t Test functions \n");
+
+            Fraction fr1 = new Fraction(1, 5);
+            Fraction fr2 = new Fraction(2,8);
+            Fraction fr3 = new Fraction(1, 4);
+            bool tmp = true;
+
+            WriteLine("1. Math and comparison operations : \n");
+            fr3 = fr1 + fr2;
+            WriteLine("\t1/5 + 2/8 = {0}", fr3.ToString("div"));
+            fr3 = fr2 - fr1;
+            WriteLine("\t2/8 - 1/5 = {0}", fr3.ToString("div"));
+            fr3 = fr2 * fr1;
+            WriteLine("\t(2/8) * (1/5) = {0}", fr3.ToString("div"));
+            fr3 = fr2 / fr1;
+            WriteLine("\t(2/8) / (1/5) = {0}", fr3.ToString("div"));
+            tmp = fr2 > fr1; // true
+            WriteLine("\t(2/8) > (1/5) - {0}", tmp);
+            tmp = fr2 < fr1; // false
+            WriteLine("\t(2/8) < (1/5) - {0}", tmp);
+            Fraction fr4 = new Fraction(1, 4);
+            tmp = (fr4 == fr2); // true
+            WriteLine("\t(1/4) == (2,8) = {0}", tmp);
+            tmp = (fr4 != fr1); // true
+            WriteLine("\t(1/4) != (1/5) = {0}", tmp);
+            Fraction fr5 = new Fraction(4,5);
+            tmp = (fr5 >= fr1);
+            WriteLine("\t(4/5) >= (1/5) = {0}", tmp);
+            tmp = (fr5 <= fr1);
+            WriteLine("\t(4/5) <= (1/5) = {0}", tmp);
+
+            WriteLine("2. Object to string in diff. formats : \n");
+            Fraction fr6 = new Fraction(10, 20);
+            WriteLine("\t(10/20) = {0}", fr6.ToString());
+            WriteLine("\t(10/20) = {0}", fr6.ToString("div"));
+            Fraction fr7 = new Fraction(3, 8);
+            WriteLine("\t(3/8) = {0}", fr7.ToString());
+            WriteLine("\t(3/8) = {0}", fr7.ToString("div"));
+            Fraction fr8 = new Fraction(1, 100);
+            WriteLine("\t(1/100) = {0}", fr8.ToString());
+            WriteLine("\t(1/100) = {0}", fr8.ToString("div"));
+
+            WriteLine("3. String to object in diff. forms : \n");
+            Fraction fr9 = Fraction.Parse("2.5");
+            Fraction fr10 = Fraction.Parse("1/4");
+            WriteLine("\tfrom 2.5 = {0}", fr9.ToString());
+            WriteLine("\tfrom 1/4 = {0}", fr10.ToString("div"));
+
+            WriteLine("4. compare and equal : \n");
+            Random rand = new Random(DateTime.Now.Millisecond);
+            List<Fraction> ourList = new List<Fraction>();
+            for (int i = 0; i<10; i++)
+            {
+                ourList.Add(new Fraction( rand.Next(5, 20), rand.Next(1, 20)));
+            }
+
+            WriteLine("\tBefore sort : ");
+            for (int i = 0; i < 10; i++)
+            {
+                Write("({0}) ", ourList[i].ToString("div"));
+            }
+            WriteLine();
+
+            ourList.Sort();
+            WriteLine("\tAfter sort : ");
+            for (int i = 0; i < 10; i++)
+            {
+                Write("({0}) ", ourList[i].ToString("div"));
+            }
+
+            Fraction fr11 = new Fraction(15, 6);
+            Fraction fr12 = new Fraction(15, 6);
+
+            tmp = fr11.Equals(fr12);
+            WriteLine("\n(15/6) equals (15/6) = {0}\n", tmp);
+
+            WriteLine("5. Try Fraction to (int) or (double) : \n");
+
+            Fraction fr20 = new Fraction(4, 3);
+            WriteLine("\t(int)4/3 = {0}", (int)fr20);
+            WriteLine("\t(double)4/3 = {0}", (double)fr20);
+
+            WriteLine("End.");
+
+            ReadKey();
+        }
+    }
+}
